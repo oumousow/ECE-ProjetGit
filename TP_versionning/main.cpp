7f86e13453039fe90e9aa1d040d433518a1ffe20
Adja Sow <adja.sow30@gmail.com>
2019-02-15 10:47:03 +0100

    modification

---
+++ TP_versionning/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  TP_versionning
+//
+//  Created by Adja Sow on 15/02/2019.
+//  Copyright © 2019 Adja Sow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout <<"projet versioooning";
+    std::cout <<"projet versioooning";
+    std::cout << "Hello, World!\n";
+    return 0;
+}
